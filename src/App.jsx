25c0769984f3690f8ad064dd28b3eca850cdbd37
nangdypanhar <181545922+nangdypanhar@users.noreply.github.com>
2025-02-02 22:47:02 +0700

    close #8 Create a side bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import "./App.css";
 import LoginPage from "./views/LoginPage/login_page";
-function App() {
+import Sidebar, { SidebarItem } from "./util/Sidebar";
+import { BsCalendar3 } from "react-icons/bs";
 
+function App() {
   return (
-   <>
-     <LoginPage />
-   </>
+    <main className="flex">
+      <Sidebar>
+        <SidebarItem icon={<BsCalendar3 size={20} />} text="Calendar"  />
+        <SidebarItem icon={<BsCalendar3 size={20} />} text="Dashboard" active />
+        <SidebarItem icon={<BsCalendar3 size={20} />} text="NewYearEvently "  />
+      </Sidebar>
+      <div>
+        hello world
+      </div>
+    </main>
   );
 }
 
