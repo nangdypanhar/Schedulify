fd8ce17bb391c7ee855b8eabea72c40b5b0f5209
nangdypanhar <181545922+nangdypanhar@users.noreply.github.com>
2025-02-09 00:44:11 +0700

    update navbar v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import "./App.css";
 import LoginPage from "./views/LoginPage/login_page";
-import Sidebar, { SidebarItem } from "./util/Sidebar";
-import { BsCalendar3 } from "react-icons/bs";
 import Navbar from "./Components/Navbar.jsx";
+import Homepage from "./views/Homepage/Homepage.jsx";
+import MySchedulePage from "./views/MySchedulePage/MySchedulePage.jsx";
+import AboutPage from "./views/AboutPage/AboutPage.jsx";
+import { Routes, Route } from "react-router-dom";
+import NotFoundPage from "./views/NotFoundPage/NotFoundPage.jsx";
 
 function App() {
   return (
-    <Navbar>
-      <main className="flex"></main>
-    </Navbar>
+    <>
+      <Navbar />
+      <main className="flex">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/myschedule" element={<MySchedulePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
---
+++ src/views/MySchedulePage/MySchedulePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MySchedulePage = () => {
+  return (
+    <div>MySchedulePage</div>
+  )
+}
+
+export default MySchedulePage
