90440f4d15e4927e13323ad5314cc43dea359769
Nangdy Panhar <181545922+nangdypanhar@users.noreply.github.com>
2025-03-24 17:24:51 +0700

    Revert "close #31 login with auth (#32)"
    This reverts commit 9b9ba538ebca52a47f48062df5648d9ba82409bb.

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import Card from "../../components/Card";
-import { fetchHomepage } from "../../service/api/HomepageData";
-import { useState, useEffect } from "react";
+import { fetchHomepage } from "../../service/HomepageData";
+import { useState,useEffect } from "react";
+
 
 const Homepage = () => {
-  const [homeData, setHomeData] = useState([]);
 
-  useEffect(() => {
-    fetchHomepage().then(setHomeData).catch(console.error);
-  }, []);
+  const [homeData,setHomeData]= useState([])
+
+  useEffect(()=>{
+    fetchHomepage().then(setHomeData).catch(console.error)
+  },[])
 
   return (
     <main className="max-w-screen-xl mx-auto p-4 flex flex-col gap-7 mt-5">
       <div className=" pb-[20px] ">
         <p className="font-bold text-xl mb-[17px]">All Schedule</p>
         <div className="mb-[20px] flex gap-4">
+          
           <select className="border border-gray-300 rounded-md w-[150px] h-[35px] text-gray-500">
             <option>Gen</option>
           </select>
-
+         
           <select className="border border-gray-300 rounded-md w-[150px] h-[35px] text-gray-500">
             <option>Specialization</option>
           </select>
 
+         
           <select className="border border-gray-300 rounded-md w-[150px] h-[35px] text-gray-500">
             <option>Group</option>
           </select>
@@ -36,6 +40,7 @@ const Homepage = () => {
           ></input>
         </div>
       </div>
+      
 
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-15 place-items-center">
         {homeData.map((schedule) => (
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,42 +1,13 @@
 import React, { useState } from "react";
 import { IoEyeOffOutline, IoEyeOutline } from "react-icons/io5";
-import { Link, useNavigate } from "react-router-dom";
-import { login as loginService } from "../../service/auth/login";
-import { useAuth } from "../../service/contexts/AuthContext";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-  const [rememberMe, setRemeberMe] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const navigate = useNavigate();
-  const { auth } = useAuth();
-  const [error, setError] = useState(null);
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const { access_token } = await loginService(formData);
-      auth.login(access_token, rememberMe);
-      navigate("/");
-    } catch (err) {
-      console.error("Login failed", err.message);
-      setError(err.message);
-    }
-  };
 
   return (
     <div className="flex flex-col justify-center items-center min-h-screen bg-[#F3F4F6] px-4 md:px-0">
+      {/* Login Container */}
       <div className="bg-white rounded-2xl w-full max-w-sm md:max-w-md lg:max-w-lg p-6 md:p-8 shadow-lg">
         <h1 className="text-2xl font-bold mb-2 text-center">
           Login to Schedulify
@@ -44,25 +15,23 @@ const LoginPage = () => {
         <p className="text-gray-500 mb-6 text-center">
           Login to stay connected
         </p>
-        {error && <div className="text-red-500 text-center mb-4">{error}</div>}
 
-        <form onSubmit={handleSubmit} className="flex flex-col gap-5">
+        {/* Form */}
+        <div className="flex flex-col gap-5">
+          {/* Username Field */}
           <div className="flex flex-col">
-            <label htmlFor="email" className="font-medium mb-2">
-              Email:
+            <label htmlFor="username" className="font-medium mb-2">
+              Username:
             </label>
             <input
               type="text"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
+              id="username"
               className="rounded-md border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter your email"
-              required
+              placeholder="Enter your name"
             />
           </div>
 
+          {/* Password Field */}
           <div className="flex flex-col">
             <label htmlFor="password" className="font-medium mb-2">
               Password:
@@ -71,12 +40,8 @@ const LoginPage = () => {
               <input
                 type={showPassword ? "text" : "password"}
                 id="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
                 className="rounded-md border border-gray-300 p-2 pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
                 placeholder="Enter your password"
-                required
               />
               <button
                 type="button"
@@ -92,36 +57,32 @@ const LoginPage = () => {
             </div>
           </div>
 
+          {/* Remember Me & Forgot Password */}
           <div className="flex flex-col md:flex-row justify-between items-center text-sm">
             <div className="flex items-center gap-2">
-              <input
-                type="checkbox"
-                id="rememberMe"
-                className="w-4 h-4"
-                checked={rememberMe}
-                onChange={(e) => setRemeberMe(e.target.checked)}
-              />
+              <input type="checkbox" id="rememberMe" className="w-4 h-4" />
               <label htmlFor="rememberMe" className="cursor-pointer">
                 Remember me
               </label>
             </div>
-            <Link
-              to="/forgot-password"
-              className="text-blue-500 hover:underline"
-            >
+            <p className="text-blue-500 hover:underline cursor-pointer">
               Forgot Password?
-            </Link>
+            </p>
           </div>
 
+          {/* Sign-In Button */}
           <div className="flex justify-center">
-            <button
-              type="submit"
-              className="bg-[#284BAD] text-white w-48 py-3 rounded-md hover:bg-blue-600 transition"
-            >
-              Sign In
-            </button>
+            <Link to="/">
+              <button
+                type="submit"
+                className="bg-[#284BAD] text-white w-48 py-3 rounded-md hover:bg-blue-600 transition"
+              >
+                Sign In
+              </button>
+            </Link>
+            {/* Sign-In Button */}
           </div>
-        </form>
+        </div>
       </div>
     </div>
   );
