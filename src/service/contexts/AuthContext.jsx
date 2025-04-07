dfb009c5541a9c5f00a1e21462cab93eb0ca8973
Sengly456 <senglyou84@gmail.com>
2025-04-07 14:55:10 +0700

    fetch homepage

--- src/service/contexts/AuthContext.jsx
+++ src/service/contexts/AuthContext.jsx
@@ -1,62 +1,62 @@
-import { jwtDecode } from "jwt-decode";
-import { createContext, useContext, useState, useEffect } from "react";
+    import { jwtDecode } from "jwt-decode";
+    import { createContext, useContext, useState, useEffect } from "react";
 
-const AuthContext = createContext();
+    const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
-  const [token, setToken] = useState(
-    localStorage.getItem("access_token") || null
-  );
-  const [user, setUser] = useState({
-    name: "",
-    email: "",
-  });
+    export const AuthProvider = ({ children }) => {
+      const [token, setToken] = useState(
+        localStorage.getItem("access_token") || null
+      );
+      const [user, setUser] = useState({
+        name: "",
+        email: "",
+      });
 
-  useEffect(() => {
-    if (token) {
-      try {
-        const decodedToken = jwtDecode(token);
-        if (decodedToken.name && decodedToken.email) {
-          setUser({
-            name: decodedToken.name,
-            email: decodedToken.email,
-          });
+      useEffect(() => {
+        if (token) {
+          try {
+            const decodedToken = jwtDecode(token);
+            if (decodedToken.name && decodedToken.email) {
+              setUser({
+                name: decodedToken.name,
+                email: decodedToken.email,
+              });
+            }
+          } catch (error) {
+            console.error("Error decoding token:", error);
+          }
         }
-      } catch (error) {
-        console.error("Error decoding token:", error);
-      }
-    }
-  }, [token]);
+      }, [token]);
 
-  const login = (access_token, rememberMe) => {
-    if (!rememberMe) {
-      sessionStorage.setItem("access_token", access_token);
-    } else {
-      localStorage.setItem("access_token", access_token);
-    }
-    setToken(access_token);
-    console.log("Logged in with token:", access_token);
-  };
+      const login = (access_token, rememberMe) => {
+        if (!rememberMe) {
+          sessionStorage.setItem("access_token", access_token);
+        } else {
+          localStorage.setItem("access_token", access_token);
+        }
+        setToken(access_token);
+        console.log("Logged in with token:", access_token);
+      };
 
-  const logout = () => {
-    localStorage.removeItem("access_token");
-    setToken(null);
-    setUser({ name: "", email: "" });
-  };
+      const logout = () => {
+        localStorage.removeItem("access_token");
+        setToken(null);
+        setUser({ name: "", email: "" });
+      };
 
-  const isAuthenticated = !!token;
+      const isAuthenticated = !!token;
 
-  const auth = {
-    token,
-    login,
-    logout,
-    isAuthenticated,
-    user,
-  };
+      const auth = {
+        token,
+        login,
+        logout,
+        isAuthenticated,
+        user,
+      };
 
-  return (
-    <AuthContext.Provider value={{ auth }}>{children}</AuthContext.Provider>
-  );
-};
+      return (
+        <AuthContext.Provider value={{ auth }}>{children}</AuthContext.Provider>
+      );
+    };
 
-export const useAuth = () => useContext(AuthContext);
+    export const useAuth = () => useContext(AuthContext);
